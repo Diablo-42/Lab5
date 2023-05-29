2f35ec3dbabc7bc8853dd33063905c25227bc5f0
Fiks Maksim <maksim.fikc@gmail.com>
2023-05-29 11:33:00 +0300

    Добавил main в xml

--- WeatherService.cpp
+++ WeatherService.cpp
@@ -2,13 +2,14 @@
 //
 
 #include <iostream>
-#include "JsonService.h"
+#include "XmlService.h"
 
 
 int main()
 {
-    JsonService js;
-    Weather w = js.getWeather("weather.json");
+    XmlService xs;
+    Weather w = xs.getWeather("weather.xml");
+
 
 }
 
